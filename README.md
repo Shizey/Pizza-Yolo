b773efc6b511f2683fd786f4a9faf7049b05339f
Shizey <adorousse@gmail.com>
2024-02-27 14:55:59 +0100

    Base code

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Pizza-Yolo
+
 Un site pour le projet du CNAM
+
+## Lancer le code
+
+```php
+php -S localhost:8000
+```
---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+    require_once 'head.html';
+?>
+<body>    
+    <h1>Hello, World</h1>
+</body>
+</html>
