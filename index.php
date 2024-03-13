f63beb5e93e250a32d1a2f0d3f4535c6b39fe3b4
Antoinedu71 <70493748+Antoinedu71@users.noreply.github.com>
2024-03-13 10:46:03 +0100

    Footer et header fini

--- index.php
+++ index.php
@@ -5,6 +5,14 @@
 ?>
 <body>
     <?php require_once 'global/header.html';  ?> 
-    <h1></h1>
+    <h1>oVo uwu :>  </h1>
+
+    <footer>
+    <?php require_once 'global/footer.html'; ?>
+</footer>
+
 </body>
+
+
+
 </html>
