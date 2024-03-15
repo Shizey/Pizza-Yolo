02ce630334545bbda91c27919cabe680b66c0bcb
Temp197 <163509397+Temp197@users.noreply.github.com>
2024-03-15 11:28:50 +0100

    ajout panier
    et modification pour fonctionnement

---
+++ panier/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<?php
+require_once '../global/head.html';
+?>
+<html>
+    <head>
+        <title>TODO supply a title</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <?php require_once '../global/header.html'; ?>
+        <h1>UWUUUUUUUUUUUUUU :> </h1>
+
+
+        <?php require_once '../global/footer.html'; ?>
+
+    </body>
+</html>
